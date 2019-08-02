4654a9a00ba038d5d06308af992faddc5aef88af
Kyle <kirvine87@gmail.com>
2019-08-02 16:17:02 +0100

    adds show customers for film

--- models/film.rb
+++ models/film.rb
@@ -43,5 +43,13 @@
      return films.map {|film| Film.new(film)}
    end
 
+   def customers()
+     sql = "SELECT customers.* FROM customers INNER JOIN tickets ON
+     customers.id = tickets.customer_id WHERE tickets.film_id = $1"
+     values = [@id]
+     customers = SqlRunner.run(sql, values)
+     return customers.map{|customer| Customer.new(customer)}
+   end
+
 
  end
